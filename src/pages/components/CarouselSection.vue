1b6656c9104339159d5393268373b99576e1b972
Yeom-JinHo <pwlsghq@naver.com>
2022-05-13 16:56:54 +0900

    feat : 각 사업장 슬라이드 추가

--- src/pages/components/CarouselSection.vue
+++ src/pages/components/CarouselSection.vue
@@ -2,27 +2,24 @@
   <div class="section" id="carousel">
     <div class="container">
       <div class="title">
-        <h4>Carousel</h4>
+        <h3>SSAFY 캠퍼스 근처 아파트 보기</h3>
+        <h5>
+          캠퍼스 이미지를 클릭하면 캠퍼스 근처 아파트 거래 현황조회가
+          가능합니다.
+        </h5>
       </div>
       <div class="row justify-content-center">
         <div class="col-8">
           <el-carousel height="500px">
-            <el-carousel-item>
-              <img class="d-block" src="img/bg1.jpg" alt="First slide" />
+            <el-carousel-item v-for="(cam, index) in campus" :key="index">
+              <img
+                class="d-block"
+                @click="moveSearch"
+                :src="cam.src"
+                :alt="cam.alt"
+              />
               <div class="carousel-caption d-none d-md-block">
-                <h5>Nature, United States</h5>
-              </div>
-            </el-carousel-item>
-            <el-carousel-item>
-              <img class="d-block" src="img/bg3.jpg" alt="Second slide" />
-              <div class="carousel-caption d-none d-md-block">
-                <h5>Somewhere Beyond, United States</h5>
-              </div>
-            </el-carousel-item>
-            <el-carousel-item>
-              <img class="d-block" src="img/bg4.jpg" alt="Third slide" />
-              <div class="carousel-caption d-none d-md-block">
-                <h5>Yellowstone National Park, United States</h5>
+                <h5>{{ cam.title }}</h5>
               </div>
             </el-carousel-item>
           </el-carousel>
@@ -32,13 +29,54 @@
   </div>
 </template>
 <script>
-import { Carousel, CarouselItem } from 'element-ui';
+import { Carousel, CarouselItem } from "element-ui";
 
 export default {
   components: {
     [Carousel.name]: Carousel,
-    [CarouselItem.name]: CarouselItem
-  }
+    [CarouselItem.name]: CarouselItem,
+  },
+  data() {
+    return {
+      campus: [
+        {
+          title: "서울캠퍼스 (멀티 스퀘어)",
+          alt: "서울캠퍼스",
+          src: "img/seoul.png",
+        },
+        {
+          title: "광주캠퍼스 (삼성전자 광주사업장)",
+          alt: "광주캠퍼스",
+          src: "img/gwangju.png",
+        },
+        {
+          title: "대전캠퍼스 (삼성화재 유성연수원)",
+          alt: "대전캠퍼스",
+          src: "img/daejeon.png",
+        },
+        {
+          title: "구미 캠퍼스 (삼성전자 구미사업장)",
+          alt: "구미 캠퍼스",
+          src: "img/gumi.png",
+        },
+        {
+          title: "부울경 캠퍼스 (삼성전기 부산사업장)",
+          alt: "부울경 캠퍼스",
+          src: "img/buulgyeong.png",
+        },
+      ],
+    };
+  },
+  methods: {
+    moveSearch() {
+      // console.log("123");
+    },
+  },
 };
 </script>
-<style></style>
+<style scoped>
+.d-block {
+  width: 100%;
+  height: 100%;
+}
+</style>
