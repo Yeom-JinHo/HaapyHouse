0b0e44288be0894c018b7b6f440ca870f0aaae52
Yeom-JinHo <pwlsghq@naver.com>
2022-05-18 00:58:13 +0900

    feat : 로그인 페이지 초기

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -7,30 +7,42 @@
     <div class="content">
       <div class="container">
         <div class="col-md-5 ml-auto mr-auto">
-          <card type="login" plain>
-            <img class="n-logo" src="img/ssafy.png" alt="" />
+          <card type="login" title="로그인" plain>
+            <img class="n-logo" slot="image" src="img/ssafy.png" alt="" />
             <div class="form-group no-border input-lg input-group">
               <div class="input-group">
-                <div class="input-group-prepend">
-                  <i class="now-ui-icons users_circle-08 input-group-text"></i>
-                </div>
-                <input class="form-control" />
-                <div class="input-group-append">
-                  <i class="now-ui-icons users_circle-08 input-group-text"></i>
-                </div>
+                <input
+                  class="form-control"
+                  placeholder="아이디를 입력해주세요.."
+                  v-model="id"
+                />
+              </div>
+              <div class="input-group input-password">
+                <input
+                  type="password"
+                  class="form-control"
+                  placeholder="비밀번호를 입력해주세요.."
+                  v-model="password"
+                />
               </div>
             </div>
-
             <template slot="raw-content">
               <div class="card-footer text-center">
-                <button class="btn btn-primary btn-round btn-lg btn-block">
-                  Get Started
+                <button
+                  @click="login"
+                  class="btn btn-primary btn-round btn-lg btn-block"
+                >
+                  <i
+                    v-if="loginWaiting"
+                    class="now-ui-icons loader_refresh"
+                  ></i>
+                  <div v-else>로그인</div>
                 </button>
               </div>
               <div class="pull-center">
                 <h6>
-                  <router-link to="/regist" class="link footer-link"
-                    >Create Account</router-link
+                  <router-link to="/join" class="link footer-link"
+                    >회원가입</router-link
                   >
                 </h6>
               </div>
@@ -53,6 +65,35 @@ export default {
     MainFooter,
     [Button.name]: Button,
   },
+  data() {
+    return {
+      id: "",
+      password: "",
+      loginWaiting: false,
+    };
+  },
+  methods: {
+    login() {
+      if (!this.loginWaiting) {
+        this.loginWaiting = true;
+        console.log("id : ", this.id, " password", this.password);
+      } else {
+        console.log("로그인 진행중입니담.. 잠시만요");
+      }
+    },
+  },
 };
 </script>
-<style></style>
+<style scoped>
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+.loader_refresh {
+  animation: spin 2s linear infinite;
+}
+</style>
